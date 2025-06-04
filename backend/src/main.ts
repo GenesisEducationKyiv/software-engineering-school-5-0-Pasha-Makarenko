c0ed834f7cc3097226b4fcccccdf9df51c4153d6
Pasha-Makarenko <pashamakarenko2007@gmail.com>
2025-06-04 22:22:51 +0300

    backend: define frontend url for cors and mail sender

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,7 @@ import * as process from "node:process"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.API_URL
-        : process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL
   })
   app.setGlobalPrefix("api")
 
